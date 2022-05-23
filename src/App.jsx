929e35fe0ada4642fa0b04eb5811cdb1a7e3562b
Henry Nawrocki <hdev@sztanski.com>
2022-05-23 17:07:51 -0500

    Update new use of createRoot to render App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <h1>Hello from the App Component</h1>;
+  return (
+    <>
+      <h1>Hello from the App Component</h1>
+      <p>Hello</p>
+    </>
+  );
 };
 
 export default App;
