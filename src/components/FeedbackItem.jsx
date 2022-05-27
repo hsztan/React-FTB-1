55604b590e3f9306cf64b7dde199a24820b2aab2
Henry Nawrocki <hdev@sztanski.com>
2022-05-27 10:00:24 -0500

    Add framer-motion and react-icons

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
+import { FaBeer } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 const FeedbackItem = () => {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState('This is an example of a feedback item.');
 
   return (
-    <div className="card">
+    <motion.div
+      animate={{ rotate: 360 }}
+      transition={{ duration: 2 }}
+      className="card"
+    >
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-    </div>
+      <div className="icon">
+        <FaBeer />
+      </div>
+    </motion.div>
   );
 };
 
