08fae233bc02b7f7a2d1477b134a401c6775058b
Henry Nawrocki <hdev@sztanski.com>
2022-05-28 09:03:12 -0500

    Final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 const App = () => {
   return (
-    <>
+    <FeedbackProvider>
       <Header bgColor="rgba(0,0,0,0.4)" text="FeedbackUI" textColor="#ff6a95" />
       <div className="container">
         <FeedbackItem />
       </div>
-    </>
+    </FeedbackProvider>
   );
 };
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,24 +1,28 @@
-import { useState } from 'react';
-import { FaBeer } from 'react-icons/fa';
-import { motion } from 'framer-motion';
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-const FeedbackItem = () => {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState('This is an example of a feedback item.');
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
 
   return (
-    <motion.div
-      animate={{ x: 100 }}
-      transition={{ ease: 'easeOut', duration: 2 }}
-      className="card"
-    >
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <div className="icon">
-        <FaBeer />
-      </div>
-    </motion.div>
-  );
-};
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
+}
 
-export default FeedbackItem;
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem
